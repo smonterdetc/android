42e6e04c0d215a063e4770b099c719917c77ce95
Vicente <vrossello@Vicentes-Mac-mini.local>
2018-07-27 14:07:47 +0200

    IOS: travelone enable auto-rotate screen y ultima versin libreria cordova ios

--- ios/TravelOne/Polyfills/cordova/platforms/ios/www/cordova_plugins.js
+++ ios/TravelOne/Polyfills/cordova/platforms/ios/www/cordova_plugins.js
@@ -1,37 +1,28 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
-    {
-        "file": "plugins/cordova-plugin-network-information/www/network.js",
-        "id": "cordova-plugin-network-information.network",
-        "pluginId": "cordova-plugin-network-information",
-        "clobbers": [
-            "navigator.connection",
-            "navigator.network.connection"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
-        "id": "cordova-plugin-network-information.Connection",
-        "pluginId": "cordova-plugin-network-information",
-        "clobbers": [
-            "Connection"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-hostedwebapp/www/hostedWebApp.js",
-        "id": "cordova-plugin-hostedwebapp.hostedwebapp",
-        "pluginId": "cordova-plugin-hostedwebapp",
-        "clobbers": [
-            "hostedwebapp"
-        ]
-    }
+  {
+    "id": "cordova-plugin-network-information.network",
+    "file": "plugins/cordova-plugin-network-information/www/network.js",
+    "pluginId": "cordova-plugin-network-information",
+    "clobbers": [
+      "navigator.connection",
+      "navigator.network.connection"
+    ]
+  },
+  {
+    "id": "cordova-plugin-network-information.Connection",
+    "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+    "pluginId": "cordova-plugin-network-information",
+    "clobbers": [
+      "Connection"
+    ]
+  }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-network-information": "2.0.1",
-    "cordova-plugin-hostedwebapp": "0.3.1"
-}
+  "cordova-plugin-network-information": "2.0.1",
+  "cordova-plugin-whitelist": "1.3.3"
+};
 // BOTTOM OF METADATA
 });
